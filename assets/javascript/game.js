6e5c8c50c5b53da60cf619a83eafaddd67654bfc
w4tsw4ts <billy.montgomery@ge.com>
2018-07-27 16:12:12 -0400

    Update v4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+// Define variables
 var wordList = ["Peugeot", "Audi", "Mercedes", "Porsche", 
                 "Volkswagen", "Ferrari", "Fiat", "Lamborghini", 
                 "Maserati", "Acura", "Datsun", "Honda", "Lexus", 
@@ -6,19 +7,56 @@ var wordList = ["Peugeot", "Audi", "Mercedes", "Porsche",
                 "Jeep", "Dodge", "Tesla", "Lincon", "Saleen", "Buick", 
                 "Cadillac", "Chrysler", "Triumph"]
 
+var validKeys = "abcdefghijklmnopqrstuvwxyz"                
+
 var lettersGussed =[];
 var match;
-var numberOfGusses = 12;
+var numberOfGusses = 7;
+var x=0;
+var guessedWord=[];
+var p;
+var s='';
 
-var computerPickWord = wordList[Math.floor(Math.random() * wordList.length)]; 
+// Pick a word from the list
+var computerPickWord = wordList[Math.floor(Math.random() * wordList.length)].toLowerCase(); 
+for (i = 0; i < length.computerPickWord; ++i) {
+    guessedWord.push("_");
+}
 
+console.log(computerPickWord);
+
+// Get key from users
 document.onkeyup = function(event) {
-    var userLetter = event.key;
-    // Already pressed?
-    if (lettersGussed.indexOf(userLetter) >-1) {
-        console.log("ALREADY GUESSED TRY AGAIN!!!")
-    } else { // New letter guessed
-        lettersGussed += userLetter;
-        console.log(lettersGussed);
+    var userLetter = event.key.toLowerCase();
+    /* console.log("user picked: " + userLetter); */
+
+    // Is KEY a valid KEY [a-z]
+    if (validKeys.indexOf(userLetter) > -1) {
+        
+        // Has the KEY been pressed before?
+        if (lettersGussed.indexOf(userLetter) > -1) {
+            // Get another KEY from user.
+            console.log("ALREADY GUESSED TRY AGAIN!!!")
+        } else if (p = computerPickWord.indexOf(userLetter) > -1) { // Does this KEY math one of the letters in the WORD?
+            guessedWord[p] = userLetter;
+            for (i = 0; i < length.guessedWord; i++) {
+                s = s + guessedWord[i];
+            }
+            console.log("MATCH: " + guessedWord); // Reveal letter(s) on the screen
+            lettersGussed += userLetter;
+            if (lettersGussed.includes(computerPickWord) > true) {
+                console.log("YOU WIN!");
+        }
+            } else {
+                lettersGussed += userLetter;
+                x++;
+                console.log("BAD Guess: " + x);
+            }
+    } else {
+        console.log("Invalid KEY")
+    }
+    // Have I run out of guesses?
+    if (x === numberOfGusses) {
+        console.log("Game Over number of guess: " + numberOfGusses);
     }
 }
